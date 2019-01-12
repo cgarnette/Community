16ed887f64a1c2123b014e0133ef699ba1e188ac
cgarnette <cgarnette18@gmail.com>
2019-01-11 22:04:57 -0500

    finally got env working

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,9 +1,12 @@
-//export const COMMUNITY_SERVICES_BASE = 'http://localhost/';
-export const COMMUNITY_SERVICES_BASE = 'http://13.58.68.184/';
-export const COMMUNITY_NEWS_ENDPOINT = COMMUNITY_SERVICES_BASE + 'community/news/';
-export const COMMUNITY_SERVICES_TWITTER_ENDPOINT = COMMUNITY_SERVICES_BASE + 'community/twitter/hashsearch/';
-export const COMMUNITY_EVENTS_EVENTBRITE_ENDPOINT = COMMUNITY_SERVICES_BASE + 'community/events/';
-export const COMMUNITY_ADMIN_ENDPOINT = COMMUNITY_SERVICES_BASE + 'community/admin/';
-export const COMMUNITY_WEATHER_ENDPOINT = COMMUNITY_SERVICES_BASE + 'community/weather/'
+require('dotenv').config();
 
-export const WEBPAGE_ADMIN = 'admin_0'
+
+export const COMMUNITY_SERVICES_BASE = process.env.REACT_APP_COMMUNITY_SERVICES_BASE || 'http://localhost/';
+//export const COMMUNITY_SERVICES_BASE = 'http://13.58.68.184/';
+export const COMMUNITY_NEWS_ENDPOINT = COMMUNITY_SERVICES_BASE + (process.env.REACT_APP_COMMUNITY_NEWS_ENDPOINT || 'community/news/');
+export const COMMUNITY_SERVICES_TWITTER_ENDPOINT = COMMUNITY_SERVICES_BASE + (process.env.REACT_APP_COMMUNITY_SERVICES_TWITTER_ENDPOINT || 'community/twitter/hashsearch/');
+export const COMMUNITY_EVENTS_EVENTBRITE_ENDPOINT = COMMUNITY_SERVICES_BASE + (process.env.REACT_APP_COMMUNITY_EVENTS_EVENTBRITE_ENDPOINT || 'community/events/');
+export const COMMUNITY_ADMIN_ENDPOINT = COMMUNITY_SERVICES_BASE + (process.env.REACT_APP_COMMUNITY_ADMIN_ENDPOINT || 'community/admin/');
+export const COMMUNITY_WEATHER_ENDPOINT = COMMUNITY_SERVICES_BASE + (process.env.REACT_APP_COMMUNITY_WEATHER_ENDPOINT || 'community/weather/');
+
+export const WEBPAGE_ADMIN = process.env.REACT_APP_WEBPAGE_ADMIN || 'admin_0';
